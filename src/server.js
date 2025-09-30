95b5a06dabda7a0d97b66e61b7faf377fcf463f3
v3ngar <66013021+v3ngar@users.noreply.github.com>
2025-09-30 23:17:21 +0300

    Updated CORS to allow traffic from Arcada

--- src/server.js
+++ src/server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3001
 const allowed = [
         'http://127.0.0.1:5501',
         'http://localhost:5501',
-        'http://localhost:8080', 
+        'http://localhost:8080',
+        'https://cgi.arcada.fi', 
     ]
 
 app.use(cors({
