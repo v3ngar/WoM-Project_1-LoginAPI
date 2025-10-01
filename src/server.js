e544fbc09ee3a8e973ba8e943c4276be1d0cc5f1
v3ngar <66013021+v3ngar@users.noreply.github.com>
2025-10-01 21:19:23 +0300

    Updated CORS to barebones

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors = require('cors')
+//const cors = require('cors')
 const morgan = require('morgan')
 require('dotenv').config()
 
@@ -7,23 +7,9 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 //CORS tillåtna origins
-const ALLOW = new Set([
-  'https://people.arcada.fi',   //live-frontend
-  'http://127.0.0.1:5501',      //lokalt
-  'http://localhost:5501',
-  'http://localhost:8080'
-])
-
-//Samma policy för både vanliga requests och preflight (OPTIONS)
-const corsOptions = {
-  origin: (origin, cb) => cb(null, !origin || ALLOW.has(origin)),
-  methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization'],
-  optionsSuccessStatus: 204
-}
-
-app.use(cors(corsOptions))
-app.options('*', cors(corsOptions)) //preflight för alla rutter
+app.use(cors({
+  origin: ['https://people.arcada.fi/']
+}))
 
 app.use(express.json())
 app.use(morgan('dev'))
