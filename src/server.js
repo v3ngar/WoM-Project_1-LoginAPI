e4ca536d59e5629d4135aa9fcb466fd6fbd233d8
v3ngar <66013021+v3ngar@users.noreply.github.com>
2025-10-01 21:43:09 +0300

    Added CORS deafult

--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
 const express = require('express')
-//const cors = require('cors')
+const cors = require('cors')
 const morgan = require('morgan')
 require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
-//CORS tillåtna origins
-//app.use(cors({
-//  origin: ['https://people.arcada.fi/']
-//}))
+
+app.use(cors())
 
 app.use(express.json())
 app.use(morgan('dev'))
