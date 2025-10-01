39cf62ed19dccc98767c22864d73a6e70bb9b456
v3ngar <66013021+v3ngar@users.noreply.github.com>
2025-10-01 21:55:11 +0300

    Frick CORS

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 
-app.use(cors({origin: '*'})) //tillåter alla origins
+app.use(cors()) //tillåter alla origins
 
 app.use(express.json())
 app.use(morgan('dev'))
